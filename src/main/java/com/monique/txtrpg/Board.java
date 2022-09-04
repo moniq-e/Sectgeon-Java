ce4d84fd54d23444efebb09bf9326774ec1a9b18
tanjirou-tk <otavioschimith124@gmail.com>
2022-09-03 21:17:47 -0300

    correção

--- src/main/java/com/monique/txtrpg/Board.java
+++ src/main/java/com/monique/txtrpg/Board.java
@@ -38,7 +38,7 @@ public class Board extends JPanel implements ActionListener, KeyListener, MouseL
     Board() {
         player = new Player(this, "default");
         cron = new Cronologia(this);
-        timer = new Timer(32, this);
+        timer = new Timer(33, this);
         timer.start();
     }
 
